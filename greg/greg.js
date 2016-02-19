570a2ac49681b46242d1ebf6e87bbe8b1a8ee981
smartYi <yiq@andrew.cmu.edu>
2016-02-19 03:48:27 -0500

    Start write a game using Javascript, html and Css

---
+++ greg/greg.js
@@ -0,0 +1,9 @@
+function getName() {
+	var name = prompt("Please enter your name: ", "");
+	document.getElementById("myname").innerHTML = name;
+}
+
+function getUsername() {
+	var name = prompt("Please enter user name: ", "");
+	document.getElementById("username").innerHTML = name;
+}
